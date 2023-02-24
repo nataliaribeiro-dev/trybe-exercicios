283398dcd82eb8e9ae5faf9fe6628e8f79a04119
natalia.ribeiro-dev <natilgr@hotmail.com>
2023-02-24 16:44:47 -0300

    execute third exercise

--- fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
+++ fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
@@ -25,13 +25,32 @@ console.log(módulo = (10 % 8));
 
 //Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
 
-let firstValue = 17;
-let secondValue = 48;
+let first = 17;
+let second = 48;
 
-if (firstValue > secondValue) {
-    console.log(firstValue + ' ' + 'is bigger than' + ' ' + secondValue + '.')
-} else if (secondValue > firstValue) {
-    console.log (secondValue + ' ' + 'is bigger than' + ' ' + firstValue + '.')
+if (first > second) {
+     console.log(first + ' ' + 'is bigger than' + ' ' + second + '.')
+} else if (second > first) {
+     console.log (second + ' ' + 'is bigger than' + ' ' + first + '.')
 } else {
-    console.log('Invalid value.')
+     console.log('Invalid value.')
 }
+
+//Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
+
+let firstValue = 1;
+let secondValue = 8;
+let thirdValue = 10;
+let message = '';
+
+if (firstValue > secondValue && thirdValue) {
+    message = firstValue + ' ' + 'é maior que'+ ' ' + secondValue + ' ' + 'e' + ' ' + thirdValue + '.';
+    console.log(message);
+} else if (secondValue > firstValue && thirdValue) {
+    message = secondValue + ' ' + 'é maior que'+ ' ' + firstValue + ' ' + 'e' + ' ' + thirdValue + '.';
+    console.log(message);
+} else if (thirdValue > firstValue || secondValue ) {
+    message = thirdValue + ' ' + 'é maior que'+ ' ' + firstValue + ' ' + 'e' + ' ' + secondValue + '.';
+    console.log(message);
+}
+
