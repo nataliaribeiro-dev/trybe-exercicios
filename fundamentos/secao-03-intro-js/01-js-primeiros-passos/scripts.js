61957735691e9eab34224e85972a432ea2685568
natalia.ribeiro-dev <natilgr@hotmail.com>
2023-02-24 14:40:15 -0300

    execute first exercice

---
+++ fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
@@ -0,0 +1,25 @@
+//Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+let a = 0;
+let b = 0;
+
+let addition = (a + b);
+console.log(addition = (10 + 8));
+
+let subtraction = (a - b);
+console.log(subtraction = (10 - 8));
+
+let multi = (a * b);
+console.log (multi = (10 * 8));
+
+let division = (a / b);
+console.log(division = (10 / 8));
+
+let remainder = (a % b);
+console.log(módulo = (10 % 8));
+
