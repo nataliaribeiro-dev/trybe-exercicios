142a013cf3ad00dba8f593461948982344aec443
natalia.ribeiro-dev <natilgr@hotmail.com>
2023-02-27 10:50:30 -0300

    execute exercise 6

--- fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
+++ fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
@@ -135,6 +135,45 @@
 //         alert ('Erro! Tente novamente.')
 // }
 
+// Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga as seguintes regras:
+// Porcentagem >= 90 -> A
+
+// Porcentagem >= 80 -> B
+
+// Porcentagem >= 70 -> C
+
+// Porcentagem >= 60 -> D
+
+// Porcentagem >= 50 -> E
+
+// Porcentagem < 50 -> F
+
+// O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+
+let nota = 82;
+let conceito = '';
+
+if (nota >= 90) {
+    nota = 'A';
+    console.log(nota);
+}else if (nota >= 80) {
+    nota = 'B';
+    console.log(nota);
+}else if (nota >= 70) {
+    nota = 'C';
+    console.log(nota);
+}else if (nota >= 60) {
+    nota = 'D';
+    console.log(nota);
+}else if (nota >= 50) {
+    nota = 'E';
+    console.log(nota);
+}else if (nota < 50) {
+    nota = 'F';
+    console.log(nota);
+}else if (nota < 0 || nota > 100) {
+    console.log('Erro! Sistema encerrado.')
+}
 
 // Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, o código deve retornar false.
 // Bônus: use somente um if.
