7e7a5dcdc7bb4bbcbe3343ebc4a358df32167dcf
natalia.ribeiro-dev <natilgr@hotmail.com>
2023-02-27 16:03:06 -0300

    finish exercises

--- fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
+++ fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
@@ -5,84 +5,84 @@
 // Divisão (a / b)
 // Módulo (a % b)
 
-// let a = 0;
-// let b = 0;
+let a = 0;
+let b = 0;
 
-// let addition = (a + b);
-// console.log(addition = (10 + 8));
+let addition = (a + b);
+console.log(addition = (10 + 8));
 
-// let subtraction = (a - b);
-// console.log(subtraction = (10 - 8));
+subtraction = (a - b);
+console.log(subtraction = (10 - 8));
 
-// let multi = (a * b);
-// console.log (multi = (10 * 8));
+let multi = (a * b);
+console.log (multi = (10 * 8));
 
-// let division = (a / b);
-// console.log(division = (10 / 8));
+let division = (a / b);
+console.log(division = (10 / 8));
 
-// let remainder = (a % b);
-// console.log(módulo = (10 % 8));
+let remainder = (a % b);
+console.log(módulo = (10 % 8));
 
 //Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas variáveis com os valores que serão comparados.
 
-// let first = 17;
-// let second = 48;
+let first = 17;
+let second = 48;
 
-// if (first > second) {
-//      console.log(first + ' ' + 'is bigger than' + ' ' + second + '.')
-// } else if (second > first) {
-//      console.log (second + ' ' + 'is bigger than' + ' ' + first + '.')
-// } else {
-//      console.log('Invalid value.')
-// }
+if (first > second) {
+    console.log(first + ' ' + 'is bigger than' + ' ' + second + '.')
+} else if (second > first) {
+    console.log (second + ' ' + 'is bigger than' + ' ' + first + '.')
+} else {
+    console.log('Invalid value.')
+}
 
 //Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três variáveis com os valores que serão comparados.
 
-// let firstValue = 1;
-// let secondValue = 8;
-// let thirdValue = 10;
-// let message = '';
+let firstValue = 1;
+let secondValue = 8;
+let thirdValue = 10;
+let message = '';
 
-// if (firstValue > secondValue && thirdValue) {
-//     message = firstValue + ' ' + 'é maior que'+ ' ' + secondValue + ' ' + 'e' + ' ' + thirdValue + '.';
-//     console.log(message);
-// } else if (secondValue > firstValue && thirdValue) {
-//     message = secondValue + ' ' + 'é maior que'+ ' ' + firstValue + ' ' + 'e' + ' ' + thirdValue + '.';
-//     console.log(message);
-// } else if (thirdValue > firstValue || secondValue ) {
-//     message = thirdValue + ' ' + 'é maior que'+ ' ' + firstValue + ' ' + 'e' + ' ' + secondValue + '.';
-//     console.log(message);
-// }
+if (firstValue > secondValue && thirdValue) {
+    message = firstValue + ' ' + 'é maior que'+ ' ' + secondValue + ' ' + 'e' + ' ' + thirdValue + '.';
+    console.log(message);
+} else if (secondValue > firstValue && thirdValue) {
+    message = secondValue + ' ' + 'é maior que'+ ' ' + firstValue + ' ' + 'e' + ' ' + thirdValue + '.';
+    console.log(message);
+} else if (thirdValue > firstValue || secondValue ) {
+    message = thirdValue + ' ' + 'é maior que'+ ' ' + firstValue + ' ' + 'e' + ' ' + secondValue + '.';
+    console.log(message);
+}
 
 //Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne: “positive”, se esse valor for positivo; “negative”, se esse valor for negativo, e “zero”, caso esse valor não seja nem positivo nem negativo.
 
-// let parameter = 3;
+let parameter = 3;
 
-// if (parameter > 0) {
-//     console.log('Positivo')
-// } else if(parameter < 0) {
-//     console.log('Negativo')
-// } else if (parameter == 0) {
-//     console.log ('Zero')
-// }
+if (parameter > 0) {
+    console.log('Positivo')
+} else if(parameter < 0) {
+    console.log('Negativo')
+} else if (parameter == 0) {
+    console.log ('Zero')
+}
 
 // Utilize if/else para escrever um código que defina três variaveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
 //Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.
 
 //Um ângulo será considerado inválido se não tiver um valor positivo.
 
-// let sideA = 20;
-// let sideB = 90;
-// let sideC = 60;
-// let sum = (sideA + sideB + sideC);
+let sideA = 20;
+let sideB = 90;
+let sideC = 60;
+let sum = (sideA + sideB + sideC);
 
-// if (sum == 180) {
-//     console.log('True');
-// } else if (sum < 180) {
-//     console.log('False')
-// } else {
-//     console.log('Error!')
-// }
+if (sum == 180) {
+    console.log('True');
+} else if (sum < 180) {
+    console.log('False')
+} else {
+    console.log('Error!')
+}
 
 //Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela pode fazer.
 //Como desafio, escreva um código para funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
@@ -97,43 +97,44 @@
 // toLowerCase is a method. So it must be called with () at the end. If you're calling the method on variable 'x', your code would be x.toLowerCase().
 // The case conditions also need to be in lowercase so that they'll match the lowercase question, for instance "Great" needs to be "great".
 
-// let answer;
-// let question = prompt('Please, name one chess piece?').toLowerCase();
+let answer;
+let question = prompt('Please, name one chess piece?').toLowerCase();
 
-// switch (question) {
-//     case 'pawn':
-//         answer = 'This chess piece can move square diagonally to the left or right.'; 
-//         alert(answer);
-//         break
+switch (question) {
+    case 'pawn':
+        answer = 'This chess piece can move square diagonally to the left or right.'; 
+        alert(answer);
+        break
 
-//     case 'bishop':
-//         answer = 'This chess piece can move diagonally as many squares as it likes.'; 
-//         alert(answer);
+    case 'bishop':
+        answer = 'This chess piece can move diagonally as many squares as it likes.'; 
+        alert(answer);
+        break
 
-//     case 'knight':
-//         answer = 'This chess piece can move one square left or right horizontally and then two squares up or down vertically.'; 
-//         alert(answer);
-//         break
+    case 'knight':
+        answer = 'This chess piece can move one square left or right horizontally and then two squares up or down vertically.'; 
+        alert(answer);
+        break
 
-//     case 'rook':
-//         answer = 'This chess piece can move as many squares as it likes left or right horizontally, or as many squares as it likes up or down vertically.'; 
-//         alert(answer);
-//         break
+    case 'rook':
+        answer = 'This chess piece can move as many squares as it likes left or right horizontally, or as many squares as it likes up or down vertically.'; 
+        alert(answer);
+        break
 
-//     case 'queen':
-//         answer = 'This chess piece can move like a rook and a bishop combined.'; 
-//         alert(answer);
-//         break
+    case 'queen':
+        answer = 'This chess piece can move like a rook and a bishop combined.'; 
+        alert(answer);
+        break
 
-//     case 'king':
-//         answer = 'This chess piece can only move one square in any direction.'; 
-//         alert(answer);
-//         break
+    case 'king':
+        answer = 'This chess piece can only move one square in any direction.'; 
+        alert(answer);
+        break
 
-//     default:
-//         answer = 'Error!';
-//         alert ('Erro! Tente novamente.')
-// }
+    default:
+        answer = 'Error!';
+        alert ('Erro! Tente novamente.')
+}
 
 // Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga as seguintes regras:
 // Porcentagem >= 90 -> A
@@ -178,15 +179,15 @@ if (nota >= 90) {
 // Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, o código deve retornar false.
 // Bônus: use somente um if.
 
-// let num1 = 2;
-// let num2 = 7;
-// let num3 = 9;
+let num1 = 2;
+let num2 = 7;
+let num3 = 9;
 
-// if(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
-//     console.log('True');
-// }else {
-//     console.log('False');
-// }
+if(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
+    console.log('True');
+}else {
+    console.log('False');
+}
 
 // Utilize if/else para escrever um código que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, o código deve retornar false.
 // Bônus: use somente um if.
