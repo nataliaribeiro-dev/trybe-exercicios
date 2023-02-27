50af0a7b70c244e1c809346ca65e11ebdf111641
natalia.ribeiro-dev <natilgr@hotmail.com>
2023-02-27 11:22:10 -0300

    execute exercise 9

--- fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
+++ fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
@@ -200,3 +200,26 @@ if(num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
 }else {
     console.log('False');
 }
+
+// Utilize if/else para escrever um código que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, o código deve calcular o lucro (ou seja, o valor de venda menos o custo do produto) da empresa ao vender mil produtos.
+// Atente para o fato de que um imposto de 20% incide sobre o custo do produto.
+
+// Seu código deve emitir uma mensagem de erro e encerrar caso algum dos valores de entrada seja menor que zero.
+
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo e deve considerar que o imposto de 20% faz parte do valor de custo.
+
+// valorCustoTotal = valorCusto + impostoSobreOCusto;
+// lucro = valorVenda - valorCustoTotal (lucro de um produto);
+
+let valorCusto = 1;
+let impostoSobreOCusto = valorCusto * 0.2;
+let valorVenda = 12;
+let valorCustoTotal = valorCusto + impostoSobreOCusto;
+let lucro;
+
+if (valorVenda > 0 && valorCusto > 0) {
+    lucro = (valorVenda - valorCustoTotal) * 1000;
+    console.log('O lucro foi de' + ' ' + lucro);
+} else if (valorVenda < 0 || valorCusto < 0) {
+    console.log('Erro! Operação encerrada.')
+}
