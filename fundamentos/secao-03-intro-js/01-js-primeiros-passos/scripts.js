b27186d9ca289d4e4c4b298407ec766f87ee6b51
natalia.ribeiro-dev <natilgr@hotmail.com>
2023-02-28 15:50:42 -0300

    execute exercises 1 and 2

---
+++ fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
@@ -0,0 +1,17 @@
+// Percorra o array imprimindo todos os valores contidos nele com a função console.log().
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]);
+// }
+
+//Some todos os valores contidos no array e imprima o resultado.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+    
+}
+console.log(sum); 
