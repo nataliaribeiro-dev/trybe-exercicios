3ad43392fa00824fc009159eee887103a9bfc29d
natalia.ribeiro-dev <natilgr@hotmail.com>
2023-03-01 08:55:21 -0300

    execute exercises

--- fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
+++ fundamentos/secao-03-intro-js/01-js-primeiros-passos/scripts.js
@@ -147,3 +147,34 @@ for(let index = word.length - 1; index >= 0; index -= 1) {
 }
 
 console.log(wordReverse);
+
+//Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
+
+
+//maior palavra
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0]; 
+
+for (index = 0; index < array.length; index += 1) {
+    if (array[index].length > biggestWord.length) {
+        biggestWord = array[index];
+    }
+}
+
+console.log(biggestWord);
+
+//menor palavra
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let smallestWord = array[0]; 
+
+for (index = 0; index < array.length; index += 1) {
+    if (array[index].length < smallestWord.length) {
+        smallestWord = array[index];
+    }
+}
+
+console.log(smallestWord);
+
